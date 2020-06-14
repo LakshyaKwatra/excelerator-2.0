bc56d9a4c3180a2e1fd5ee08ca358592df158123
LakshyaKwatra <lkwatra2@gmail.com>
2020-06-14 20:23:10 +0530

    more form fields added

--- compare/forms.py
+++ compare/forms.py
@@ -2,19 +2,32 @@ from django import forms
 from . import views
 from upload.models import Upload
 
+DEFAULT_CHOICE = '-----'
+FILTER_CHOICES = [('View All', 'View All'),
+                  ('View Unique', 'View Unique'),
+                  ('View Same', 'View Same')]
 
 class ComparisonForm(forms.Form):
     file1sheet = forms.ChoiceField(choices=[])
     file1column = forms.ChoiceField(choices=[])
     file2sheet = forms.ChoiceField(choices=[])
     file2column = forms.ChoiceField(choices=[])
+    pivot_column = forms.ChoiceField(choices=[])
+    filters = forms.ChoiceField(choices=FILTER_CHOICES)
 
     def __init__(self, *args, **kwargs):
         super(ComparisonForm, self).__init__(*args, **kwargs)
         self.fields['file1sheet'] = forms.ChoiceField(choices=self.get_choices_dict()['file1sheet_choices'])
+        self.fields['file1sheet'].label = "First File Sheet"
         self.fields['file1column'] = forms.ChoiceField(choices=self.get_choices_dict()['file1column_choices'])
+        self.fields['file1column'].label = "First File Column"
         self.fields['file2sheet'] = forms.ChoiceField(choices=self.get_choices_dict()['file2sheet_choices'])
+        self.fields['file2sheet'].label = "Second File Sheet"
         self.fields['file2column'] = forms.ChoiceField(choices=self.get_choices_dict()['file2column_choices'])
+        self.fields['file2column'].label = "Second File Column"
+        self.fields['pivot_column'] = forms.ChoiceField(choices=self.get_choices_dict()['pivot_column_choices'])
+        self.fields['pivot_column'].label = "Pivot Column"
+        self.fields['filters'].label = "Apply Filter"
 
     def get_choices_dict(self):
         object_data = views.get_object_data(Upload.objects.last())
@@ -26,29 +39,35 @@ class ComparisonForm(forms.Form):
         file2dropdown_dict = object_data['file2dropdown_dict']
         file1_is_xl = object_data['file1_is_xl']
         file2_is_xl = object_data['file2_is_xl']
-        FILE1SHEET_CHOICES = [('---', '---')]
-        FILE2SHEET_CHOICES = [('---', '---')]
-        FILE1COLUMN_CHOICES = [('---', '---')]
-        FILE2COLUMN_CHOICES = [('---', '---')]
+        FILE1SHEET_CHOICES = [(DEFAULT_CHOICE, DEFAULT_CHOICE)]
+        FILE2SHEET_CHOICES = [(DEFAULT_CHOICE, DEFAULT_CHOICE)]
+        FILE1COLUMN_CHOICES = [(DEFAULT_CHOICE, DEFAULT_CHOICE)]
+        FILE2COLUMN_CHOICES = [(DEFAULT_CHOICE, DEFAULT_CHOICE)]
+        PIVOT_COLUMN_CHOICES = [('No Pivot Required', 'No Pivot Required')]
         if file1_is_xl:
             for sheet in file1sheets:
                 FILE1SHEET_CHOICES.append((sheet, sheet))
                 for column in file1dropdown_dict[sheet]:
                     FILE1COLUMN_CHOICES.append((column, column))
+                    PIVOT_COLUMN_CHOICES.append(('File-1: '+column, 'File-1: '+column))
         else:
             for column in file1dropdown:
                 FILE1COLUMN_CHOICES.append((column, column))
+                PIVOT_COLUMN_CHOICES.append(('File-1: '+column, 'File-1: '+column))
         if file2_is_xl:
             for sheet in file2sheets:
                 FILE2SHEET_CHOICES.append((sheet, sheet))
                 for column in file2dropdown_dict[sheet]:
                     FILE2COLUMN_CHOICES.append((column, column))
+                    PIVOT_COLUMN_CHOICES.append(('File-2: '+column, 'File-2: '+column))
         else:
             for column in file2dropdown:
                 FILE2COLUMN_CHOICES.append((column, column))
+                PIVOT_COLUMN_CHOICES.append(('File-2: '+column, 'File-2: '+column))
         context = {}
         context['file1sheet_choices'] = FILE1SHEET_CHOICES
         context['file2sheet_choices'] = FILE2SHEET_CHOICES
         context['file1column_choices'] = FILE1COLUMN_CHOICES
         context['file2column_choices'] = FILE2COLUMN_CHOICES
+        context['pivot_column_choices'] = PIVOT_COLUMN_CHOICES
         return context
